1ddadb32dfa33e3d42e74108389d7264d4164fb3
David Smith <goldstar.cleareye@proton.me>
2025-01-18 18:31:48 +0000

    Initial comit

---
+++ README.md
@@ -0,0 +1,9 @@
+use nvm 16
+
+ganache
+
+truffle
+
+metamask
+
+reference : dappuniversiti.org ( blochchain tutorial for deveolpers )
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./components/App.js";
+import * as serviceWorker from "./serviceWorker";
+const glowDB = require('luma-glow-db');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
