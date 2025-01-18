9855bba10677bb2bcaebaafb17a0b60f6b43950d
David Smith <goldstar.cleareye@proton.me>
2025-01-18 18:31:48 +0000

    Initial comit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  globals: {
+    artifacts: "readonly", // Recognizes artifacts globally
+    contract: "readonly",
+    assert: "readonly",
+    web3: "readonly",
+  },
+  // Other ESLint configurations...
+};
